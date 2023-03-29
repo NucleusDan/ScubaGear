acd9b68516f95a16bf051a5613039caec052795a
Richard Crutchfield <crutchfield@mitre.org>
2023-03-29 13:49:05 -0400

    WIP

--- Testing/Functional/Report.Tests.ps1
+++ Testing/Functional/Report.Tests.ps1
@@ -1,20 +1,22 @@
 Import-Module Selenium
 
-Describe -Tag "UI","Chrome" -Name "Test Report with Chrome" {
+Describe -Tag "UI","Chrome" -Name "Test Report with <Browser>" -ForEach @(
+    @{ Browser = "Chrome"; Driver = Start-SeChrome 2>$null }
+    @{ Browser = "Edge"; Driver = Start-SeEdge 2>$null }
+){
 	BeforeAll {
-        $script:url = "file:///C:/Users/crutchfield/source/repos/ScubaGear/M365BaselineConformance_2023_03_02_09_24_54/BaselineReports.html"
-		$script:ChromeDriver = Start-SeChrome -Arguments "headless", "incognito"
-        Enter-SeUrl $script:url -Driver $script:ChromeDriver
+        $script:url = "file:///C:/Users/crutchfield/source/repos/ScubaGear/Testing/M365BaselineConformance_2023_03_24_08_41_48/BaselineReports.html"
+        Enter-SeUrl $script:url -Driver $Driver 2>$null
 	}
 
 	It "Toggle Dark Mode" {
-        $ToggleCheckbox = Find-SeElement -Driver $script:ChromeDriver -By XPath "//input[@id='toggle']"
-        $ToggleText = Find-SeElement -Driver $script:ChromeDriver -Id "toggle-text"
+        $ToggleCheckbox = Find-SeElement -Driver $Driver -Wait -By XPath "//input[@id='toggle']"
+        $ToggleText = Find-SeElement -Driver $Driver -Wait -Id "toggle-text"
 
         $ToggleCheckbox.Selected | Should -Be $false
         $ToggleText.Text | Should -Be 'Light Mode'
 
-        $ToggleSwitch = Find-SeElement -Driver $script:ChromeDriver -ClassName "switch"
+        $ToggleSwitch = Find-SeElement -Driver $Driver -Wait -ClassName "switch"
         Invoke-SeClick -Element $ToggleSwitch
 
         $ToggleText.Text | Should -Be 'Dark Mode'
@@ -22,7 +24,7 @@ Describe -Tag "UI","Chrome" -Name "Test Report with Chrome" {
 	}
 
     It "Verify Tenant"{
-        $TenantDataElement = Find-SeElement -Driver $script:ChromeDriver -ClassName "tenantdata"
+        $TenantDataElement = Find-SeElement -Driver $Driver -Wait -ClassName "tenantdata"
         $TenantDataRows = Find-SeElement -Target $TenantDataElement -By TagName "tr"
         $TenantDataColumns = Find-SeElement -Target $TenantDataRows[1] -By TagName "td"
         $Tenant = $TenantDataColumns[0].Text
@@ -30,24 +32,28 @@ Describe -Tag "UI","Chrome" -Name "Test Report with Chrome" {
     }
 
     It "Verify  Domain"{
-        $TenantDataElement = Find-SeElement -Driver $script:ChromeDriver -ClassName "tenantdata"
+        $TenantDataElement = Find-SeElement -Driver $Driver -Wait -ClassName "tenantdata"
         $TenantDataRows = Find-SeElement -Target $TenantDataElement -By TagName "tr"
         $TenantDataColumns = Find-SeElement -Target $TenantDataRows[1] -By TagName "td"
         $Domain = $TenantDataColumns[1].Text
         $Domain | Should -Be "cisaent.onmicrosoft.com" -Because "Domain is $Domain"
     }
 
-    It "Goto Azure Active Directory details"{
-        $AadLink = Find-SeElement -Driver $script:ChromeDriver -By LinkText "Azure Active Directory"
-        Invoke-SeClick -Element $AadLink
+    It "Goto <Product> (<LinkText>) details" -ForEach @(
+        @{Product = "aad"; LinkText = "Azure Active Directory"}
+        @{Product = "onedrive"; LinkText = "OneDrive for Business"}
+    ){
+        $DetailLink = Find-SeElement -Driver $Driver -Wait -By LinkText $LinkText
+        $DetailLink | Should -Not -BeNullOrEmpty
+        Invoke-SeClick -Element $DetailLink
 
-        $ToggleCheckbox = Find-SeElement -Driver $script:ChromeDriver -By XPath "//input[@id='toggle']"
-        $ToggleText = Find-SeElement -Driver $script:ChromeDriver -Id "toggle-text"
+        $ToggleCheckbox = Find-SeElement -Driver $Driver -Wait -By XPath "//input[@id='toggle']"
+        $ToggleText = Find-SeElement -Driver $Driver -Wait -Id "toggle-text"
 
         $ToggleText.Text | Should -Be 'Dark Mode'
         $ToggleCheckbox.Selected | Should -Be $true
 
-        $ToggleSwitch = Find-SeElement -Driver $script:ChromeDriver -ClassName "switch"
+        $ToggleSwitch = Find-SeElement -Driver $Driver -Wait -ClassName "switch"
         Invoke-SeClick -Element $ToggleSwitch
 
         $ToggleText.Text | Should -Be 'Light Mode'
@@ -55,83 +61,14 @@ Describe -Tag "UI","Chrome" -Name "Test Report with Chrome" {
     }
 
     It "Go Back to main page - Is Dark mode in correct state"{
-        Open-SeUrl -Back -Driver $script:ChromeDriver
-        $ToggleCheckbox = Find-SeElement -Driver $script:ChromeDriver -By XPath "//input[@id='toggle']"
-        $ToggleText = Find-SeElement -Driver $script:ChromeDriver -Id "toggle-text"
+        Open-SeUrl -Back -Driver $Driver
+        $ToggleCheckbox = Find-SeElement -Driver $Driver -Wait -By XPath "//input[@id='toggle']"
+        $ToggleText = Find-SeElement -Driver $Driver -Wait -Id "toggle-text"
         $ToggleText.Text | Should -Be 'Light Mode'
         $ToggleCheckbox.Selected | Should -Be $false
     }
 
 	AfterAll {
-		Stop-SeDriver -Driver $script:ChromeDriver
-	}
-}
-
-Describe -Tag "UI","Edge" -Name "Test Report with Edge" {
-	BeforeAll {
-        $script:url = "file:///C:/Users/crutchfield/source/repos/ScubaGear/M365BaselineConformance_2023_03_02_09_24_54/BaselineReports.html"
-		$script:EdgeDriver = Start-SeEdge
-        Enter-SeUrl $script:url -Driver $script:EdgeDriver
-	}
-
-	It "Toggle Dark Mode" {
-        $ToggleCheckbox = Find-SeElement -Driver $script:EdgeDriver -By XPath "//input[@id='toggle']"
-        $ToggleText = Find-SeElement -Driver $script:EdgeDriver -Id "toggle-text"
-
-        $ToggleCheckbox.Selected | Should -Be $false
-        $ToggleText.Text | Should -Be 'Light Mode'
-
-        $ToggleSwitch = Find-SeElement -Driver $script:EdgeDriver -ClassName "switch"
-        Invoke-SeClick -Element $ToggleSwitch
-
-        $ToggleText.Text | Should -Be 'Dark Mode'
-        $ToggleCheckbox.Selected | Should -Be $true
-	}
-
-    It "Verify Tenant"{
-        $TenantDataElement = Find-SeElement -Driver $script:EdgeDriver -ClassName "tenantdata"
-        $TenantDataRows = Find-SeElement -Target $TenantDataElement -By TagName "tr"
-        $TenantDataColumns = Find-SeElement -Target $TenantDataRows[1] -By TagName "td"
-        $Tenant = $TenantDataColumns[0].Text
-        $Tenant | Should -Be "Cybersecurity and Infrastructure Security Agency" -Because $Tenant
-    }
-
-    It "Verify  Domain"{
-        $TenantDataElement = Find-SeElement -Driver $script:EdgeDriver -ClassName "tenantdata"
-        $TenantDataRows = Find-SeElement -Target $TenantDataElement -By TagName "tr"
-        $TenantDataColumns = Find-SeElement -Target $TenantDataRows[1] -By TagName "td"
-        $Domain = $TenantDataColumns[1].Text
-        $Domain | Should -Be "cisaent.onmicrosoft.com" -Because "Domain is $Domain"
-    }
-
-    It "Goto Azure Active Directory details"{
-        $AadLink = Find-SeElement -Driver $script:EdgeDriver -By LinkText "Azure Active Directory"
-        Invoke-SeClick -Element $AadLink
-
-        $ToggleCheckbox = Find-SeElement -Driver $script:EdgeDriver -By XPath "//input[@id='toggle']"
-        $ToggleText = Find-SeElement -Driver $script:EdgeDriver -Id "toggle-text"
-
-        $ToggleText.Text | Should -Be 'Dark Mode'
-        $ToggleCheckbox.Selected | Should -Be $true
-
-        $ToggleSwitch = Find-SeElement -Driver $script:EdgeDriver -ClassName "switch"
-        Invoke-SeClick -Element $ToggleSwitch
-
-        $ToggleText.Text | Should -Be 'Light Mode'
-        $ToggleCheckbox.Selected | Should -Be $false
-    }
-
-    It "Go Back to main page - Is Dark mode in correct state"{
-        Open-SeUrl -Back -Driver $script:EdgeDriver
-
-        $ToggleCheckbox = Find-SeElement -Driver $script:EdgeDriver -By XPath "//input[@id='toggle']"
-        $ToggleText = Find-SeElement -Driver $script:EdgeDriver -Id "toggle-text"
-
-        $ToggleText.Text | Should -Be 'Light Mode'
-        $ToggleCheckbox.Selected | Should -Be $false
-    }
-
-	AfterAll {
-		Stop-SeDriver -Driver $script:EdgeDriver
+		Stop-SeDriver -Driver $Driver 2>$null
 	}
 }
